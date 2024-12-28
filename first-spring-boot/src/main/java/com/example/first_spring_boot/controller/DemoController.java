7dad2d0181de6a9d4d895ff65f167ffa414dab01
MaxChou1205 <v2asault@yahoo.com.tw>
2024-12-28 23:36:24 +0800

    change field injection into construct injection (better for unit test)

--- first-spring-boot/src/main/java/com/example/first_spring_boot/controller/DemoController.java
+++ first-spring-boot/src/main/java/com/example/first_spring_boot/controller/DemoController.java
@@ -9,14 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class DemoController {
-  @Autowired
-  private Coach coach;
 
-  // @Autowired
-  // public DemoController(Coach coach) {
-  // this.coach = coach;
-  // }
-  @GetMapping("/getCoach")
+  private final Coach coach;
+
+  @Autowired
+  public DemoController(Coach coach) {
+    this.coach = coach;
+  }
+
+  @GetMapping("/getCoach" )
   public String getCoach() {
     return coach.getDailyWorkout();
   }
--- first-spring-boot/src/main/java/com/example/util/CricketCoach.java
+++ first-spring-boot/src/main/java/com/example/util/CricketCoach.java
@@ -8,5 +8,4 @@ public class CricketCoach implements Coach {
   public String getDailyWorkout() {
     return "Practice fast bowling for 15 minutes!!";
   }
-
 }
